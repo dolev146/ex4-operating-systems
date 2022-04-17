ca441f5656606b45894cc67729ac9884555ccf52
dolev146 <dolev146@gmail.com>
2022-04-17 14:30:11 -0700

    need yakov look

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-//
+#include <stdio.h>
+#include <unistd.h>
 
-int main(){
-    
+int main()
+{
+    int pid = fork();
+    if (pid == 0)
+    {
+        char *args[] = {"./server", NULL};
+        execv(args[0], args);
+    }
+    printf("parent");
+
     return 0;
 }
--- makefile
+++ makefile
@@ -1,4 +1,14 @@
-all: main.o
+run: all
+
+all: client.o ThreadServer.o
+	./server
 
 main.o: main.cpp
 	g++ -o main main.cpp 
+
+client.o: client.c
+	gcc client.c -o client
+
+ThreadServer.o: ThreadServer.c
+	gcc ThreadServer.c -o server -lpthread
+
